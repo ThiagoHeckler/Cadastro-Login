cae2283a32fc5718430d87e1bfbd898f446195fe
ThiagoHeckler <thiag0224@hotmail.com>
2024-09-29 10:01:56 -0300

    Atualizações web

--- Cadastro&Login/script/script.js
+++ Cadastro&Login/script/script.js
@@ -7,3 +7,13 @@ fetch('../api/dados')
     .then(data => {
         console.log(data);
     });
+
+    function showLogin() {
+        document.getElementById('loginForm').style.display = 'block';
+        document.getElementById('cadastroForm').style.display = 'none';
+    }
+    
+    function showCadastro() {
+        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('cadastroForm').style.display = 'block';
+    }
